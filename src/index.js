0b91298b1227bd5a7f76f3bdc7cc60bdfb078835
Maxim Khvatalin <khmm12@gmail.com>
2017-04-07 21:36:36 +0300

    change output format to RoR like, extract code to helpers functions

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import chalk from 'chalk'
 export default function knexTinyLogger (knex, { logger = console.log } = {}) {
   const queries = {}
   knex.on('query', ({ sql, bindings, __knexQueryUid: queryId }) => {
-    const startTime = process.hrtime()
+    const startTime = executionTime()
     queries[queryId] = { sql, bindings, startTime }
   })
   .on('query-error', (_error, { __knexQueryUid: queryId }) => {
@@ -22,18 +22,49 @@ export default function knexTinyLogger (knex, { logger = console.log } = {}) {
     const { sql, bindings, startTime } = queries[queryId]
     delete queries[queryId]
 
-    const diff = process.hrtime(startTime)
-    const duration = diff[0] * 1e3 + diff[1] * 1e-6
-    const sqlRequest = sql.split('?').reduce((memo, part, index) => {
-      const binding = bindings[index] ? bindings[index] : ''
-      return memo + part + binding
-    }, '')
+    const duration = executionTime(startTime)
+    const sqlRequest = insertBindingsToSQL(sql, bindings)
 
-    logger('%s %s %s',
-      chalk.gray('SQL'),
-      chalk.cyan(sqlRequest),
-      chalk.magenta(duration.toFixed(3) + 'ms')
+    logger('%s %s',
+      chalk.magenta(`SQL (${duration.toFixed(3)} ms)`),
+      chalk.cyan(sqlRequest)
     )
   })
   return knex
+}
+
+/**
+ * Return SQL string with inserted bindings
+ *
+ * @param {String} sql - sql string without bindings
+ * @param {Array} bindings
+ * @return {String}
+ */
+
+function insertBindingsToSQL (sql, bindings) {
+  return sql.split('?').reduce((memo, part, index) => {
+    const binding = bindings[index] ? bindings[index] : ''
+    return memo + part + binding
+  }, '')
+}
+
+/**
+ * Return duration in ms based `startTime`
+ *
+ * @example
+ * const startTime = executionTime()
+ * const duration = executionTime(startTime)
+ *
+ * @param {Object} [startTime]
+ * @return {Number} duration in ms
+ */
+
+function executionTime (startTime) {
+  if (startTime) {
+    const diff = process.hrtime(startTime)
+    const duration = diff[0] * 1e3 + diff[1] * 1e-6
+    return duration
+  } else {
+    return process.hrtime()
+  }
 }
